318b2cfdfd6af8f4b37faec0acd42a74d284c35d
Ibrahim Yilmaz <0ibrahimyilmaz@gmail.com>
2023-05-12 17:05:22 +0200

    add example project

---
+++ ShakeReportExample/ShakeReportExample/ShakeReportExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShakeReportExampleApp.swift
+//  ShakeReportExample
+//
+//  Created by Ibrahim Yilmaz on 12.05.23.
+//
+
+import SwiftUI
+
+@main
+struct ShakeReportExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
