b686da251d0f809fe5a2b4a008041cf57929d33f
Ufuk Türközü <57141872+ufuk-trkz@users.noreply.github.com>
2023-05-13 19:11:38 +0200

    Update UI

--- Sources/ShakeReport/FloatingPanel/ShakeReportFloatingView.swift
+++ Sources/ShakeReport/FloatingPanel/ShakeReportFloatingView.swift
@@ -28,11 +28,15 @@ struct ShakeReportFloatingView: View {
     
     @State private var isMinimized: Bool = false
     private var offsetLeading: CGFloat {
-        isMinimized ? 0 : 150
+        isMinimized ? 70 : 0
     }
     
     private var buttonImageName: String {
-        isMinimized ? "chevron.compact.right" : "chevron.compact.left"
+        isMinimized ? "chevron.compact.left" : "chevron.compact.right"
+    }
+    
+    private var chevronForegroundColor: Color {
+        isMinimized ? .accentColor : .secondary.opacity(0.5)
     }
     
     var body: some View {
@@ -43,38 +47,69 @@ struct ShakeReportFloatingView: View {
             } label: {
                 Image(systemName: buttonImageName)
                     .font(.system(size: 40))
-                    .foregroundColor(.accentColor)
+                    .foregroundColor(chevronForegroundColor)
             }
             .frame(width: 25, height: 80)
             .background(.ultraThinMaterial)
             .cornerRadius(14, corners: [.topLeft, .bottomLeft])
             
-            VStack(spacing: 10) {
-                Button("Take a screenshot") {
+            VStack(spacing: 16) {
+                Button {
                     viewModel.takeScreenshot()
+                } label: {
+                    Image(systemName: "camera")
                 }
                 
-                Button("Record a video") {
+                Divider()
+                
+                Button {
                     viewModel.recordVideo()
+                } label: {
+                    Image(systemName: "video")
                 }
                 
-                Button("Send JIRA report") {
+                Divider()
+                
+                Button {
                     viewModel.sendReport()
+                } label: {
+                    Image(systemName: "paperplane")
                 }
                 
-                Button("Show Logs") {
+                Divider()
+                
+                Button {
                     viewModel.showLogs()
+                } label: {
+                    Image(systemName: "terminal")
                 }
                 
-                Button("Close") {
-                    
+                Divider()
+                
+                Button {
+                    isMinimized = true
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.7) {
+                        ShakeReport.close()
+                    }
+                } label: {
+                    Image(systemName: "multiply")
+                        .foregroundColor(.red)
                 }
             }
-            .frame(width: 150, height: 150)
+            .padding(.horizontal, 16)
+            .padding(.vertical, 20)
+            .frame(width: 70)
             .background(.ultraThinMaterial)
             .cornerRadius(14)
+            .font(.system(size: 20))
         }
         .offset(x: offsetLeading)
         .animation(.default, value: offsetLeading)
+    }
+}
+
+struct ShakeReportFloatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShakeReportMainView(viewModel: ShakeReportMainViewModelImpl(reportingService: MockedReportingService()))
     }
 }
--- Sources/ShakeReport/ShakeReport.swift
+++ Sources/ShakeReport/ShakeReport.swift
@@ -9,11 +9,13 @@ public struct ShakeReport {
         guard let currentWindow = UIWindow.current, currentWindow.viewWithTag(viewTag) == nil else { return }
         
         let floatingViewHosting = UIHostingController(rootView: floatingView)
+        floatingViewHosting.view.backgroundColor = .clear
         
-        let width = 175.0
-        let height = 150.0
-        let xOffset = currentWindow.bounds.width - width
-        let yOffset = currentWindow.bounds.height - height - 100.0
+        let intrinsicContentSize = floatingViewHosting.view.intrinsicContentSize
+        let width = intrinsicContentSize.width
+        let height = intrinsicContentSize.height
+        let xOffset = currentWindow.bounds.width - width - 4
+        let yOffset = currentWindow.bounds.height - height - 150.0
         floatingViewHosting.view.frame = CGRect(x: xOffset, y: yOffset, width: width, height: height)
         floatingViewHosting.view.tag = viewTag
         currentWindow.addSubview(floatingViewHosting.view)
